1f1da1173508fbd0a58b1a495d1d289aff8b997e
Daniel OFarrell <ofarreld@onid.oregonstate.edu>
2015-04-30 23:34:16 +0900

    Finished multtable.php

---
+++ src/multtable.php
@@ -0,0 +1,99 @@
+<?php
+/* *************************************************************
+* PHP 
+* 
+* Name: 								Daniel Ofarrell
+* Filename:								phpPrint
+* Date Created:							30 April 2015
+* Last Modification Date:				30 April 2015
+*
+* Description:
+*	Will produce a table from information gained from assign04.html (in testing)
+* Input:
+*	Checks if min < max in both multiplicand and multipliers
+*	Also checks if all 4 values are numbers
+* Output:
+*	Produces a multiplication table of all values. For example:
+*	  1 2 3
+*	2 2 4 6
+*	3 3 6 9	
+*
+* ************************************************************* */
+?>
+
+<?php
+
+	// assign values sent from html form
+	$minMultiplicand = $_GET["min-multiplicand"];
+	$maxMultiplicand = $_GET["max-multiplicand"];
+	$minMultiplier = $_GET["min-multiplier"];
+	$maxMultiplier = $_GET["max-multiplier"];
+	
+	$inputErrorCand = false;
+	$inputErrorIer = false;
+	
+	// checks return address and removes current page
+	$returnAddress = explode( '/', $_SERVER['PHP_SELF'], -1);
+	$returnAddress = implode('/', $returnAddress);
+	$returnAddress .= "/assign04.html";
+
+	
+	// error handling on input
+	if ($minMultiplicand > $maxMultiplicand) {
+		$inputErrorCand = true;
+		echo "<br>Minimum multiplicand larger than maximum. <br>";
+		echo "<a href=' ".$returnAddress." '>Click here to try again</a>";
+	}
+	if ($minMultiplier > $maxMultiplier) {
+		$inputErrorIer = true;
+		echo "<br>Minimum multiplier larger than maximum. <br>";
+		echo "<a href=' ".$returnAddress." '>Click here to try again</a>";
+	}
+	
+	// checks if input values are numeric values
+	// (could be shorter if written as an array?)
+	if (!is_numeric($minMultiplier)) {
+		$inputErrorIer = true;
+		notInt($minMultiplier, $returnAddress);
+	}
+	if (!is_numeric($minMultiplicand)) {
+		$inputErrorIer = true;
+		notInt($minMultiplicand, $returnAddress);
+	}
+	if (!is_numeric($maxMultiplicand)) {
+		$inputErrorIer = true;
+		notInt($maxMultiplicand, $returnAddress);
+	}
+	if (!is_numeric($maxMultiplier)) {
+		$inputErrorIer = true;
+		notInt($maxMultiplier, $returnAddress);
+	}
+	
+	//not int output
+	function notInt(&$invalidInput, $returnAddressnotInt){		
+		echo "<br>I'm sorry, but $invalidInput is not an integer. <br>";
+		echo "<a href=' ".$returnAddressnotInt." '>Click here to try again</a>";		
+	}
+	
+	if (!$inputErrorIer && !$inputErrorCand) {
+		echo "<table border=\"2\">";
+		echo "<td></td>";
+		// print the multiplipliers
+		for ($top = $minMultiplier; $top < ($maxMultiplier + 1); $top++) {
+			echo '<td>' .$top. '</td>';
+		}
+		echo '<tr>';
+		// print table data
+		for ($col = $minMultiplicand; $col < ($maxMultiplicand + 1); $col++) {	
+			// print multiplicant
+			echo '<td>' .$col. '</td>';
+			// print table
+			for ($row = $minMultiplier; $row < ($maxMultiplier + 1); $row++) {	
+				echo '<td>' .$row*$col. '</td>';
+			}
+			echo "<tr>";		
+		}
+		echo "</table>";
+	} 
+?>
+
